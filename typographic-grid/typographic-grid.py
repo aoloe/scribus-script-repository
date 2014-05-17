f382039ed28c124d9457fcaaa47249360b8573fd
Blue Prawn <monnier.florent@gmail.com>
2014-05-17 19:07:30 +0200

    wrote setGuidesRow() based on setGuidesColumn()

--- typographic-grid/typographic-grid.py
+++ typographic-grid/typographic-grid.py
@@ -26,7 +26,22 @@ def setGuidesColumn(n, gap):
     scribus.setVGuides(guide)
 
 def setGuidesRow(n, gap):
-    print "TODO: implement setGuidesRow"
+    print "setGuidesRow should be a scribus API function"
+    (margin_top, margin_left, margin_right, margin_bottom) = scribus.getPageNMargins(scribus.currentPage())
+    print "left " + str(margin_left)
+    print "right " + str(margin_right)
+    (width, height) = scribus.getPageNSize(scribus.currentPage())
+    print "width " + str(width)
+    print "height " + str(height)
+    cell_width = (width - (margin_left + margin_right) - (gap * (n - 1))) / n
+    print cell_width
+    guide = []
+    previous_guide = margin_left
+    for i in range(0, n - 1):
+        guide.append(previous_guide + cell_width)
+        guide.append(previous_guide + cell_width + gap)
+        previous_guide = previous_guide + cell_width + gap
+    scribus.setHGuides(guide)
 
 unit_current=scribus.getUnit() #get unit and change it to mm
 scribus.setUnit(scribus.UNIT_MILLIMETERS)
@@ -37,7 +52,7 @@ margin = scribus.getPageMargins()
 # scribus.setHGuides([10,20,30])
 
 setGuidesColumn(6, 5)
-# setGuidesRow(4, 5)
+setGuidesRow(4, 5)
 
 
 
