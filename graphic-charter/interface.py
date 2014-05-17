26a07e8d6f2e619b2f65e84966d42cd06a78693e
Christophe Schockaert <R3vLibre@citadels.be>
2014-05-17 18:03:38 +0200

    Add class to define the graphical chart, as colors and fonts.

--- graphic-charter/interface.py
+++ graphic-charter/interface.py
@@ -18,12 +18,12 @@
 ###############################################################################
 
 # List of color kinds used to identify colors within Scribus
-color_kinds = ["rgb", "cmyb", "pantone"]
+color_kinds = ["rgb", "cmyk", "pantone"]
 
 class color_def:
     """Class for color definition: 
-  - kind: color kind (rgb, cmyb, pantone)
-  - ref: reference (rgb code, cmyb code, or pantone reference)
+  - kind: color kind (rgb, cmyk, pantone)
+  - ref: reference (rgb code, cmyk code, or pantone reference)
   - label: label used to identify the color within Scribus
 """
     def __init__(kind, ref, label):
@@ -41,4 +41,19 @@ class font_def:
         self.name = name
         self.style = style
         return
+
+class graphic_chart_def:
+    """Class for graphic chart definition:
+  - colors (color_def): list of colors in graphic chart
+  - fonts (font_def): list of fonts in graphic chart
+"""
+    def __init__():
+        self.colors = []
+        self.fonts = []
+
+    def add_color(self, color):
+        self.colors.append(color)
+
+    def add_font(self, font):
+        self.fonts.append(font)
 
