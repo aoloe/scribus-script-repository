8d34ed00ab70beec760e53391cd34e956eba1985
Ale Rimoldi <ale@graphicslab.org>
2015-09-14 20:14:29 +0200

    create a pdf based on the current file. can be used from the command line

---
+++ to-pdf/README.md
@@ -0,0 +1,3 @@
+Produces a PDF for the SLA passed as a parameter.
+
+Uses the same file name and replaces the .sla extension with .pdf
---
+++ to-pdf/to-pdf.py
@@ -0,0 +1,15 @@
+# Produces a PDF for the SLA passed as a parameter.
+# Uses the same file name and replaces the .sla extension with .pdf
+#
+# usage:
+# scribus -g -py to-pdf.py file.sla
+
+import os
+
+if scribus.haveDoc() :
+    filename = os.path.splitext(scribus.getDocName())[0]
+    pdf = scribus.PDFfile()
+    pdf.file = filename+".pdf"
+    pdf.save()
+else :
+    print("No file open")
