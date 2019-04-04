703a7a0991108556e2755c304e4f668adeabe2ca
ale rimoldi <ale@graphicslab.org>
2019-04-04 13:23:28 +0200

    first working draft for the syntax highlight script

---
+++ syntax-highlight/README.md
@@ -0,0 +1,59 @@
+# Syntax highlighter
+
+Highlight the code in a frame.
+
+## Install
+
+Before running the script, you need to install the Python 2 version of [Pygments](http://pygments.org/).
+
+You can install it with your package manager (in my case it did not pull in any dependency) or with Pip.
+
+Using virtual environment will probably not work (well), since you will need to run Scribus after having activated it.
+
+## Usage
+
+Select the text frame with the code to be highlighted and run the script.
+
+## Notes
+
+### The basic tokens to get started
+
+Here is how a simple code is parsed into tokens:
+
+
+```py
+print("Hello World")
+```
+
+- Token.Keyword: print
+- Token.Punctuation: () :
+- Token.Literal.String.Double: " stringcontent
+- Token.Text: space
+
+```py
+for i in range(3):
+    print('Hello World')
+```
+
+- Token.Operator.Word: in
+- Token.Name.Builtin: for
+- Token.Literal.Number.Integer: 3
+
+## Todo
+
+- [ ] Actually read the content of the frame
+- [ ] Optionally read the language from the item attributes
+- [ ] Make sure that the characte styles are reset before setting the new ones.
+- [ ] Make sure that a text frame is selected.
+- [ ] Allow highlighting a text selection.
+- [ ] Make sure that the character style `Code` is defined
+- [ ] Create the colors used by the highlither
+  - Their names should be prefixed with `Code_`
+- [ ] Create the character styles used for the specific code snippet
+  - [ ] Create the styles with the default formatting defined by Pygments
+  - [ ] Base the new styles on the `Code` style (needs a patch for `createCharStyle()`).
+- [ ] Complete the matching between token types and styles
+  - We don't want one style for each possible token type
+- [ ] any way to create this as a c++ plugin?
+  - https://doc.qt.io/qt-5/qtwidgets-richtext-syntaxhighlighter-example.html
+	- but, then, i will probably have to recreate the lexers
---
+++ syntax-highlight/syntax-highlight.py
@@ -0,0 +1,107 @@
+import sys
+from pygments import highlight
+
+try: 
+    import scribus
+except ImportError: 
+    import mockAPI
+    scribus = mockAPI.Scribus()
+
+from pygments.formatter import Formatter
+from pygments.token import Token
+# from pygments.token import Token, Text, STANDARD_TYPES
+
+
+class ScribusFormatter(Formatter):
+    # TODO: we could give the user a way to change this... or not?
+    base_char_style = 'Code'
+    def __init__(self, code_length, **options):
+        Formatter.__init__(self, **options)
+        # css classes are "defined" in https://github.com/dagwieers/pygments/blob/d4425131d42a69651d27260e45bdfd68f29f06ad/pygments/token.py#L124
+        # TODO: we can use this to create a list of styles, intialize them to the values in style and create them in scribus on first actual usage
+        self.code_length = code_length
+        print(self.code_length)
+        self.character_styles = {
+            Token.Text: self.base_char_style,
+            Token.Keyword: self.base_char_style+'_keyword',
+            Token.Punctuation: self.base_char_style+'_punctuation',
+            Token.Literal.String.Double: self.base_char_style+'_literal',
+            Token.Literal.String.Single: self.base_char_style+'_literal',
+
+            Token.Name: self.base_char_style,
+            Token.Operator.Word: self.base_char_style+'_keyword',
+            Token.Name.Builtin: self.base_char_style+'_stdlib',
+            Token.Literal.Number.Integer: self.base_char_style+'_literal',
+        }
+        self.existing_styles = scribus.getCharStyles()
+        if self.base_char_style not in self.existing_styles:
+            scribus.createCharStyle(self.base_char_style)
+        # for token, style in self.style:
+        #    pass
+
+    def format(self, tokensource, outfile):
+        current_frame = scribus.getSelectedObject()
+        # TODO: also support formatting a selection as soon as the API has getTextSelection -> (start, length)
+        # resete the character styles
+        scribus.setCharacterStyle(self.base_char_style)
+
+        pos = 0
+        last_val = ''
+        last_type = None
+        for token_type, token_value in tokensource:
+            print(token_type)
+            print(token_value)
+            token_length = len(token_value)
+            # scribus does not have a \n after the last character,
+            # the code has it
+            if pos + token_length > self.code_length:
+                token_length -= 1
+            # print(pos)
+            # print(token_length)
+            scribus.selectText(pos, token_length, current_frame)
+            pos += token_length
+            style_name = self.get_char_style(token_type)
+            scribus.setCharacterStyle(style_name, current_frame)
+
+    def get_char_style(self, token_type):
+        style_name = self.character_styles.get(token_type, self.base_char_style)
+        print(self.existing_styles)
+        if style_name not in self.existing_styles:
+            # TODO: self.style[token] contains a "standard" definition for the style
+            print('adding style '+style_name)
+            self.existing_styles.append(style_name)
+            # #TODO: Create our own colors (except black)
+            # defineColorRGB("name", r, g, b) 
+            # TODO: add the parent_style to the API
+            scribus.createCharStyle(style_name)
+
+        return style_name
+
+# TODO: read the text from the frame
+
+print('=====')
+
+# TODO: read the item's attributes
+attribute = 'python'
+
+if attribute != '':
+    try:
+        from pygments.lexers import get_lexer_by_name
+        lexer = get_lexer_by_name(attribute)
+    except pygments.util.ClassNotFound:
+        pass
+else:
+    from pygments.lexers import guess_lexer
+    try:
+        lexer = guess_lexer(code)
+    except pygments.util.ClassNotFound:
+        pass
+
+if not lexer:
+    sys.exit()
+
+# TODO: do nothing is the lexer is 'text' (guess_lexer did not find anything better)
+# print(lexer)
+
+code = scribus.getAllText()
+highlight(code, lexer, ScribusFormatter(len(code)))
