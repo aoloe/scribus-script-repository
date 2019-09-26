a3e583bbe31d86add26ab3c23648ce65935ba5b1
ale rimoldi <ale@graphicslab.org>
2019-09-26 16:46:38 +0200

    add a script that loads images in existing frames

--- load-images/README.md
+++ load-images/README.md
@@ -1,9 +1,25 @@
-# Load images
+# Loading images
+
+This directory contains a few scripts that automate the loading of images in your document.
+
+## `load-images.py`
 
 Load all images from a directory, one image per page.
 
 It starts from the selected frame and creates an image of the frame size on each one of the following pages and loads the next image (alphabetically).
 
-If the end of the document is reached, new pages are automatically creqated.
+If the end of the document is reached, new pages are automatically created.
 
-Warning: at the time of writing, the script cannot create new pages, if the document has been created in one language (french) and is being edited in another (english). In that case, You have to create enough pages before running the script (you can always remove the exceeding ones afterwards).
+Warning: at the time of writing, if the document has been created in one language (french) and is being edited in a differnt one (english), there is a bug in scribus that makes it impossible for the script to create new pages.
+
+In that case, You have to create enough pages before running the script (you can always remove the exceeding ones afterwards).
+
+## `load-images-1.5-untested.py`
+
+An untested version of `load-images.py` that copies the selected frame in the new pages instead of creating new frames.
+
+## `load-images-existing-fames.py`
+
+Load all images from a directory into the empty image frames in the current document.
+
+You need to create enough (empty) image frames before running the script (see `edit > multiduplicate` for ways of duplicating your frames).
---
+++ load-images/load-images-existing-fames.py
@@ -0,0 +1,54 @@
+# - get the directory with all images
+# - run the script
+#
+# - in a document add empty image frames
+# - all empty image frames will be filled with the images collected
+import os
+import sys
+import scribus
+
+if not scribus.haveDoc():
+    scribus.messagebarText("No .")
+    sys.exit()
+
+def get_all_images(path):
+    extensions = ['jpg', 'JPG', 'png', 'PNG', 'tif', 'TIF']
+    filenames = [f for f in os.listdir(path)
+                 if any(f.endswith(ext) for ext in extensions)]
+    filenames.sort()
+    return filenames
+
+def get_all_empty_images_frames():
+    image_frames = []
+    for page in range(1, scribus.pageCount() + 1):
+        page_image_frames = []
+        scribus.gotoPage(page)
+        # get all empty image frames on the page
+        for item in scribus.getPageItems():
+            if item[1] == 2:
+                if scribus.getImageFile(item[0]) == "":
+                    x, y = scribus.getPosition(item[0])
+                    page_image_frames.append((item[0], x, y))
+        # sort the frames by position
+        page_image_frames.sort(key=lambda k: [k[2], k[1]])
+        image_frames += [i[0] for i in page_image_frames]
+    return image_frames
+
+path = scribus.fileDialog("Pick a directory", scribus.getDocName(), isdir = True)
+if path == '':
+   scribus.messagebarText("No directory selected.")
+
+images = get_all_images(path)
+if not images:
+    scribus.messagebarText("No image found.")
+    sys.exit()
+
+print(images)
+
+frames = get_all_empty_images_frames()
+print(frames)
+
+
+for frame, image in zip(frames, images):
+    scribus.loadImage(os.path.join(path, image), frame)
+    # scribus.setScaleImageToFrame(True, True, new_image)
