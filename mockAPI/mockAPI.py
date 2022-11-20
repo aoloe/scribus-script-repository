559447ed9ca1e5ba9ced67a802b596dc731a93fb
ale rimoldi <ale@graphicslab.org>
2022-11-20 10:22:58 +0100

    a few more mocks

--- mockAPI/mockAPI.py
+++ mockAPI/mockAPI.py
@@ -1,4 +1,9 @@
-class Scribus :
+class Scribus:
+    def __init__(self):
+        self.mock_instance = True
+        self.items = {}
+        self.selection = []
+
     UNIT_MILLIMETERS = 0
     ICON_WARNING = 0
     ICON_NONE = 0
@@ -21,7 +26,7 @@ class Scribus :
     def currentPage(self) :
         return 1
     def getPageNMargins(self, n) :
-        return (14.1111111111, 14.1111111111, 14.1111111111, 14.1111111111) 
+        return (14.1111111111, 14.1111111111, 14.1111111111, 14.1111111111)
     def getPageNSize(self, n) :
         return (210.0, 297.0)
     def setVGuides(self, guides) :
@@ -38,7 +43,38 @@ class Scribus :
         pass
     def createCharStyle(self, style_name, **keywords):
         pass
-    def getSelectedObject(self):
-        return 'item'
 
-# scribus = Scribus()
+    def createText(self, x, y, w, h, name=None):
+        if name is None:
+            name = f'Text{len(self.items) + 1}'
+        item = MockPageItem('Text', x, y, w, h, name)
+        self.items[name] = item
+    def createLine(self, x1, y1, x2, y2, name=None):
+        if name is None:
+            name = f'Line{len(self.items) + 1}'
+        item = MockPageItem('Line', x1, y2, x2 - x1, y2 - y1, name)
+        self.items[name] = item
+
+    def getPosition(self, name):
+        return self.items[name].x, self.items[name].y
+    def getSize(self, name):
+        return self.items[name].w, self.items[name].h
+
+    def selectionCount(self):
+        return len(self.selection)
+    def getSelectedObject(self, i):
+        return self.selection[i]
+
+    def mock_inject_items(self, items):
+        self.selection = items
+    def mock_inject_selection(self, items):
+        self.selection = items
+
+class MockPageItem:
+    def __init__(self, itemType, x, y, w, h, name=None):
+        self.itemType = itemType
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.name = name
