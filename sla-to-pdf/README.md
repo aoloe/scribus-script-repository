6aca2f559c0f0ace6596acbee3d38455e6f1d335
ale rimoldi <ale@graphicslab.org>
2022-11-20 10:21:44 +0100

    somehow better, but in the future we should just use pdfjam to do the whole job

--- sla-to-pdf/README.md
+++ sla-to-pdf/README.md
@@ -1,3 +1,41 @@
 # Impose a .sla as Pdf
 
-`sla-to-pdf.py` produces both "straight" Pdf files and imposed Pdfs, ready to be printed on a home or office printer.
+`sla-to-pdf.py` reads a A5 or A6 portrait Scribus document and produces:
+
+- a PDF with the current settings in the Scribus file;
+- with the `-booklet` option, it also produces an A4 Pdf that can be printed both sides to create a simple booklet;
+- with the `-impose` option, it also produces an A4 Pdf to be printed single sided (contact print);
+
+> Please, hae a look at the _Imposition_ script in this repository. It's probably a better solution.
+
+## Introduction
+
+The script should be started from the command line, and it will launch Scribus before running the script with the given options.
+
+You can run it with the `-h` option to get more information about its usage.
+
+In order to run the script you need Scribus (1.5+) and pdfjam installed (on Debian it's part of `texlive-extra-utils`).
+
+## Page count
+
+You can only create booklets if the number of pages is a multiple of 4.  
+The only exceptions are 2 pages document, where the `-booklet` produces and imposed duplex A4 Pdf
+
+## Notes
+
+### Adding a full page cover
+
+In order to create a full page cover and all other pages imposed from an A5/A6 created for booklet printing:
+
+- Create the Pdf with the cover:
+  - Save your document as `document-cover.sla`
+  - Change the document page size to A4 (landscape for A5)
+  - Modify the layout of the cover to match the new size.
+  - Create the `document-cover.pdf`
+- Create a new Pdf with all the pages but the cover (and possibly another page to get an even number of pages).  
+  You can use `pdfjam` to extract the pages:  
+  `pdfjam document.pdf 2- document.pdf -o document-nocover.pdf`
+- Create the nup Pdf:  
+  `pdfjam --nup 2x1 --landscape --a4paer --outfile document-a4-nocover.pdf document-nocover.pdf`
+- Join the A4 cover with the A4 pdf:  
+  `pdfjam document-cover.pdf document-a4-nocover.pdf --nup 1x1 --landscape --a4paper --outfile document-a4.pdf`
--- sla-to-pdf/sla-to-pdf.py
+++ sla-to-pdf/sla-to-pdf.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# -booklet: only available from 4 pages up
 
 import sys
 import os
 import argparse
 import subprocess
+import itertools
 
 debug = False
 
@@ -28,15 +28,98 @@ def run_scribus() :
             call_args = ['scribus', '-g', '-py', sys.argv[0]]
             if args.booklet:
                 call_args += ['-booklet']
+            if args.impose:
+                call_args += ['-impose']
                 # call_args += ['-pa', '-booklet']
             call_args += ['--', file.name]
             subprocess.call(call_args)
     else:
         pass
 
+# def impose_a6(filename):
+#     # create a pdf with the page reordered
+#     # get it to be steared by facing pages and first page left / right
+#     # it's booklet if it has facing pages and first page right
+#     # (the function is probably not in the API yet)
+#     n = scribus.pageCount()
+#     pdf = scribus.PDFfile()
+#     pdf.file = filename + "-reordered.pdf"
+#     if (n == 1) :
+#         pdf.pages = [1,1,1,1]
+#     elif (n == 2) :
+#         pdf.pages = [1,1,1,1,2,2,2,2]
+#     elif (n == 4) :
+#         if args.booklet:
+#             pdf.pages = [4,1,4,1,2,3,2,3]
+#         else:
+#             pdf.pages = [1,3,1,3,4,2,4,2]
+#     elif (n == 8) :
+#         if args.booklet:
+#             pdf.pages = [8,1,6,3,2,7,4,5]
+#         else:
+#             pdf.pages = [1,3,5,7,4,2,8,6]
+#     elif (n == 12) :
+#         if args.booklet:
+#             pdf.pages = [12, 1, 2, 11, 10, 3, 4, 9, 8, 5, 6, 7]
+#         else :
+#             pass
+#     elif (n == 16) :
+#         if args.booklet:
+#             pdf.pages = [16, 1, 14 , 3, 2, 15, 4, 13, 12, 5, 10, 7, 6, 11, 8, 9] # <- first join top/down , then staple 1/2
+#         else:
+#             pass
+#     elif (n == 24) :
+#         if args.booklet:
+#             pass
+#         else:
+#             pdf.pages = [1,3,5,7,4,2,8,6,9,11,13,15,12,10,16,14,17,19,21,23,20,18,24,22] # mit starter cards
+#     else:
+#         print('{} are not yet supported'.format(n))
+#     pdf.save()
+#
+#     call_args = ['pdfnup', '--nup', '2x2', '--frame', 'false', '--no-landscape']
+#     call_args += ['--', 'outfile', filename.replace("a6", "a4") + ".pdf"]
+#     call_args += [filename + '-reordered.pdf']
+#     subprocess.call(call_args)
+#
+#     os.system("pdfnup --nup 2x2 --frame false --no-landscape " + filename + "-reordered.pdf --outfile " + filename.replace("a6", "a4") + ".pdf")
+#     os.remove(pdf.file)
+
+def impose_a5(filename):
+    call_args = ['pdfjam', '--nup', '2x1', '--frame', 'false', '--landscape', '--a4paper']
+    call_args += ['--outfile', filename.replace("A5", "a4") + ".pdf"]
+    call_args += [filename + '.pdf']
+    subprocess.call(call_args)
+
+def booklet_a5(filename):
+    # create a pdf with the page reordered
+    # get it to be steared by facing pages and first page left / right
+    # it's booklet if it has facing pages and first page right
+    # (the function is probably not in the API yet)
+    n = scribus.pageCount()
+    pdf = scribus.PDFfile()
+    pdf.file = filename + "-reordered.pdf"
+    if n == 4:
+        pdf.pages = [] # to be tested
+    elif n % 4 == 0:
+        pdf.pages = get_booklet_order(n)
+    else:
+        print('{} are not yet supported'.format(n))
+
+    print('>>>>', pdf.pages)
+
+    pdf.save()
+
+    # TODO: use a regex to replace ([-_]?[Aa][56]) by nothing and then add -a4 to the end
+    call_args = ['pdfjam', '--nup', '2x1', '--frame', 'false', '--landscape', '--a4paper']
+    call_args += ['--outfile', filename.replace("A5", "a4") + "-booklet.pdf"]
+    call_args += [filename + '-reordered.pdf']
+    subprocess.call(call_args)
+
+    os.remove(filename + '-reordered.pdf')
+
 def run_script() :
     args = get_args_scribus()
-    print('booooklet ' + str(args.booklet))
 
     if not scribus.haveDoc():
         print("No file open");
@@ -50,53 +133,41 @@ def run_script() :
     pdf.file = filename + ".pdf"
     pdf.save()
 
-    # create a pdf with the page reordered
-    # get it to be steared by facing pages and first page left / right
-    # it's booklet if it has facing pages and first page right
-    # (the function are probably not in the API yet)
-    n = scribus.pageCount()
-    pdf = scribus.PDFfile()
-    pdf.file = filename + "-reordered.pdf"
-    if (n == 1) :
-        pdf.pages = [1,1,1,1]
-    elif (n == 2) :
-        pdf.pages = [1,1,1,1,2,2,2,2]
-    elif (n == 4) :
-        if args.booklet:
-            pdf.pages = [4,1,4,1,2,3,2,3]
-        else:
-            pdf.pages = [1,3,1,3,4,2,4,2]
-    elif (n == 8) :
-        if args.booklet:
-            pdf.pages = [8,1,6,3,2,7,4,5]
-        else:
-            pdf.pages = [1,3,5,7,4,2,8,6]
-    elif (n == 12) :
-        if args.booklet:
-            pdf.pages = [12, 1, 2, 11, 10, 3, 4, 9, 8, 5, 6, 7]
-        else :
-            pass
-    elif (n == 16) :
-        if args.booklet:
-            pdf.pages = [16, 1, 14 , 3, 2, 15, 4, 13, 12, 5, 10, 7, 6, 11, 8, 9] # <- first join top/down , then staple 1/2
-        else:
-            pass
-    elif (n == 24) :
-        if args.booklet:
-            pass
-        else:
-            pdf.pages = [1,3,5,7,4,2,8,6,9,11,13,15,12,10,16,14,17,19,21,23,20,18,24,22] # mit starter cards
+    page_size =  tuple(round(x,1) for x in scribus.getPageSize())
+    if page_size == (148.0, 210.0):
+       if args.booklet:
+           booklet_a5(filename)
+       if args.impose:
+           print('heyyyy')
+           impose_a5(filename)
+    elif page_size == (105.0, 148.0):
+        # impose_a6(filename)
+        pass
     else:
-        print('{} are not yet supported'.format(n))
-    pdf.save()
+        print('uknown page size')
+        pdf = scribus.PDFfile()
+        pdf.file = filename + "unknown.pdf"
+        pdf.save()
 
-    call_args = ['pdfnup', '--nup', '2x2', '--frame', 'false', '--no-landscape']
-    call_args += ['--', 'outfile', filename.replace("a6", "a4") + ".pdf"]
-    call_args += [filename + '-reordered.pdf']
-    subprocess.call(call_args)
+def get_booklet_order(page_count):
+    if page_count % 4 != 0:
+        return []
 
-    os.system("pdfnup --nup 2x2 --frame false --no-landscape " + filename + "-reordered.pdf --outfile " + filename.replace("a6", "a4") + ".pdf")
-    os.remove(pdf.file)
+    numbering = []
+
+    sheet_count = page_count // 4
+
+    j = 1
+    for i in range(sheet_count):
+        numbering.append([page_count - j + 1, j, j + 1, page_count - j])
+        j += 2
+
+    numbering = list(itertools.chain(*numbering))
+    # numbering = [j for i in numbering for j in i]
+
+    return numbering
+
+
 
 
 def get_args_terminal():
@@ -104,6 +175,9 @@ def get_args_terminal():
 
     parser.add_argument('-booklet', action='store_true',
         help='impose as a booklet')
+
+    parser.add_argument('-impose', action='store_true',
+        help='impose as multiple pages on a sheet')
 
     parser.add_argument('file', type=argparse.FileType('r'), nargs='*',
         default=None,
@@ -119,6 +193,9 @@ def get_args_scribus():
 
     parser.add_argument('-booklet', action='store_true',
         help='impose as a booklet')
+
+    parser.add_argument('-impose', action='store_true',
+        help='impose as multiple pages on a sheet')
 
     parser.add_argument('-debug', action='store_true',
         help='output debug information')
