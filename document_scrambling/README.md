3db99b979c9f1f697cd3af5d4e72516435c5ee53
ale rimoldi <ale@graphicslab.org>
2023-11-18 13:31:06 +0100

    rename to new schema

---
+++ document_scrambling/README.md
@@ -0,0 +1,39 @@
+# Scrambling a document
+
+This script scrambles the content of a scribus document:
+
+- text is scrambled by shuffling the letters of the document without losing the words rythm and the formatting.
+- images are blurred at a very high level.
+
+If there is a selection, only the selected frames are scrambled.
+
+Don't forget to only run the script on a copy of your document! Otherwise, you risk to lose your work.
+
+# Requirements
+
+- For blurring the images, you need ImageMagick installed
+- Scribus 1.5 with the patches uploaded in the tickets: 12253, 12583.
+
+# How does it work
+
+- Get the fields to be processed:
+  - If there is a selection, only the selected fields are processed and the list of selected fields is recorded.
+  - Only the text and image fields are retained.
+- The eventual current selection is cancelled.
+- Go through all the text frames and collect all the characters that are not on the list to be ignored.
+- Shuffle the collected characters
+- For each text frame:
+  - Loop through all the characters,
+  - put the next shuffled character before it,
+  - by retaining the case,
+  - and delete the current caracter.
+- For each image frame:
+  - Use ImageMagick's `identify` to get the image size.
+  - Make a copy of the linked image with a very strong blur (with sigma defined as size divided by 50).
+  - Load the blurred image into the image frame.
+- Restore the initial selection.
+
+
+# Todo:
+
+- correctly handle inline images (if needed)
---
+++ document_scrambling/document_scrambling.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# © 2014, MIT license, Ale Rimoldi <a.l.e@graphicslab.org>
+"""
+USAGE
+ 
+You must have a document open, and at least one text frame selected.
+ 
+"""
+import scribus
+import random
+import os
+import distutils.spawn
+ 
+if not scribus.haveDoc():
+    scribus.messageBox('Usage Error', 'You need a Document open', icon=0, button1=1)
+    sys.exit(2)
+
+charsIgnore = ["-", "–", "­", " ", ".", ":", ";", ";", "?", "!", "\n", "'", "‘", "’", "," "\"", "“", "”", "\r"]
+
+button = scribus.messageBox('Confirmation', 'You should only scramble a copy of your document', scribus.ICON_WARNING, scribus.BUTTON_OK, scribus.BUTTON_CANCEL)
+
+if button == scribus.BUTTON_CANCEL:
+    sys.exit(2)
+
+selectedFrame = []
+textFrames = []
+imageFrames = []
+if scribus.selectionCount() == 0:
+    for page in range(scribus.pageCount()):
+        scribus.gotoPage(page + 1)
+        scribus.messagebarText("Processing Page "+str(page))
+        scribus.redrawAll()
+        for item in scribus.getPageItems():
+            if (item[1] == 4):
+                textFrames.append(item[0])
+            elif (item[1] == 2):
+                imageFrames.append(item[0])
+else:
+    for i in range(scribus.selectionCount()):
+        item = scribus.getSelectedObject(i)
+        selectedFrame.append(item)
+        if scribus.getObjectType(item) == "TextFrame":
+            textFrames.append(item)
+        elif scribus.getObjectType(item) == "ImageFrame":
+            imageFrames.append(item)
+
+# print(textFrames)
+# print(imageFrames)
+
+scribus.deselectAll()
+chars = []
+for item in textFrames:
+    scribus.deselectAll()
+    scribus.selectObject(item)
+    n = scribus.getTextLength()
+    for i in range(n):
+        scribus.selectText(i, 1)
+        char = scribus.getText()
+        if (char not in charsIgnore and len(char) > 0):
+            chars.append(char.lower())
+
+random.shuffle(chars)
+
+for item in textFrames:
+    scribus.messagebarText("Processing text frame "+item)
+    scribus.redrawAll()
+    # print(item)
+    scribus.deselectAll()
+    scribus.selectObject(item)
+    n = scribus.getTextLength()
+    # print(n)
+    for i in range(n):
+        scribus.selectText(i, 1)
+        original = scribus.getText()
+        if original not in charsIgnore and len(original) > 0:
+            shuffled = chars.pop(0)
+            if original.isupper():
+                shuffled = shuffled.upper()
+            scribus.insertText(shuffled, i)
+            scribus.selectText(i + 1, 1)
+            scribus.deleteText()
+
+for item in imageFrames:
+    # print('image frame', item)
+    scribus.messagebarText("Processing image frame "+item)
+    scribus.redrawAll()
+    imageFile = scribus.getImageFile(item)
+    if imageFile == '':
+        continue
+    fileName, fileExtension = os.path.splitext(imageFile)
+    imageFileBlurred = fileName+"_blurred"+fileExtension
+    # TODO: instead of using image magick we should expos scribus' own blur to the scripter
+    if distutils.spawn.find_executable("convert") != "":
+        # print('img', imageFile)
+        command = "identify "+imageFile
+        result = os.popen(command).read();
+        if result != "":
+            size = max(result.split(" ")[2].split("x")) # extract max(width, height)
+            # print('size', size)
+            command = "convert "+imageFile+" -blur 0x"+str(int(size)/50)+" "+imageFileBlurred
+            # print  command
+            os.system(command)
+            scribus.loadImage(imageFileBlurred, item)
+
+scribus.deselectAll()
+for item in selectedFrame:
+    scribus.selectObject(item)
+ 
+scribus.messagebarText("Finished, The text has been shuffled")
+scribus.setRedraw(1)
+scribus.docChanged(1)
+
