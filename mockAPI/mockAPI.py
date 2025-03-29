6921e90716f63df71aedf8093db4f17e2029f65b
ale rimoldi <ale@graphicslab.org>
2025-03-29 14:39:07 +0100

    ...

--- mockAPI/mockAPI.py
+++ mockAPI/mockAPI.py
@@ -3,6 +3,12 @@ class Scribus:
         self.mock_instance = True
         self.items = {}
         self.selection = []
+        
+        self.docName = ''
+        self.page = 1
+        self.page_counter = 1
+        self.column_guides = {'number': 0, 'gap': 0, 'reference': 0, 'guides': []}
+        self.row_guides = {'number': 0, 'gap': 0, 'reference': 0, 'guides': []}
 
     UNIT_MILLIMETERS = 0
     ICON_WARNING = 0
@@ -12,14 +18,17 @@ class Scribus:
     BUTTON_NO = 0
 
     def haveDoc(self) :
-        return True
+        return self.docName != ''
+    def getDocName(self) :
+        return self.docName
     def openDoc(self, name) :
         pass
     def closeDoc(self) :
         pass
-    def messageBox(self, title, description,  icon, button1 = 0, button2 = 0, button3 = 0) :
+    def messageBox(self, title, description,  icon=ICON_NONE, button1=0, button2=0, button3=0) :
+        print(f'{title}:\n{description}')
         return button1
-    def valueDialog(self, title, description, default = '') :
+    def valueDialog(self, title, description, default='') :
         return default
     def setRedraw(self, activate) :
         return True
@@ -27,8 +36,14 @@ class Scribus:
         return True
     def setUnit(self, unit) :
         return True
+    def newPage(self):
+        self.page_counter += 1
+    def pageCount(self):
+        return self.page_counter
     def currentPage(self) :
-        return 1
+        return self.page
+    def gotoPage(self, page):
+        self.page = page
     def getPageNMargins(self, n) :
         return (14.1111111111, 14.1111111111, 14.1111111111, 14.1111111111)
     def getPageNSize(self, n) :
@@ -37,6 +52,22 @@ class Scribus:
         return True
     def setHGuides(self, guides) :
         return True
+    def getColumnGuides(self):
+        return self.column_guides
+    def getRowGuides(self):
+        return self.row_guides
+    def setColumnGuides(self, number, gap=None, refer_to=0):
+        self.column_guides = {
+            'number': number,
+            'gap': gap if gap != None else self.column_guides['gap'],
+            'refer_to': refer_to if refer_to != None else self.column_guides['refer_to'],
+        }
+    def setRowGuides(self, number, gap=0.0, refer_to=0):
+        self.row_guides = {
+            'number': number,
+            'gap': gap if gap != None else self.row_guides['gap'],
+            'refer_to': refer_to if refer_to != None else self.row_guides['refer_to'],
+        }
     def setBaseLine(self, line_height_mm, margin_top) :
         return True
     def selectText(self, start, end, frame) :
