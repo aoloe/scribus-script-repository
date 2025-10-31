a7c827d589eabbc93f826b536a4acbfa97de84da
ale rimoldi <ale@graphicslab.org>
2025-10-31 07:37:43 +0100

    first tries with opencv...

--- image-contour-detect/README.md
+++ image-contour-detect/README.md
@@ -3,3 +3,10 @@
   - A main panel for the image
 - A complex bezier drawer  
   <https://github.com/ankit-4129/bezier-curve-drawer/blob/main/src/beziercurve.py>
+
+
+
+- Visualisation of the different contours examples: <https://docs.opencv.org/3.4/dc/dcf/tutorial_js_contour_features.html>
+- Explaination of the different methods: <https://github.com/torlar/opencv-python-examples/blob/main/08_contours/README.md>
+- A few thoughts about unions, intersections and contours: <https://technicqa.com/when-to-use-union-of-contours-in-opencv/>
+- shapely might help with intersections and unions...
--- image-contour-detect/main.py
+++ image-contour-detect/main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 from PIL import ImageTk, Image
 
+import cv2
+import numpy as np
+
 def cancel(root):
     root.quit()
     root.withdraw()
@@ -21,20 +24,81 @@ def main():
     p1.add(p_left)
 
 
-    def action_A():
-        print('action 1')
-    def action_B():
-        print('action 2')
+    def action_A(input_filename, output_filename):
+        img = cv2.imread(input_filename, cv2.IMREAD_UNCHANGED)
 
-    button_1_tk = tk.Button(p_left, text='A', command=action_A)
+        #convert img to grey
+        # img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img_grey = cv2.cvtColor(img,cv2.COLOR_RGBA2GRAY)
+        cv2.imwrite('/tmp/grey.png',img_grey)
+        #set a thresh
+        thresh = 100
+        #get threshold image
+        ret,thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
+        #find contours
+        contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        #create an empty image for contours
+        img_contours = np.zeros(img.shape)
+        # draw the contours on the empty image
+        cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+        #save image
+        cv2.imwrite(output_filename,img_contours)
+
+    def action_B(input_filename, output_filename):
+        img = cv2.imread(input_filename, cv2.IMREAD_UNCHANGED)
+
+        #convert img to grey
+        img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        # thresh = 100
+        # ret,thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
+
+        # blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        canny = cv2.Canny(img_grey, 30, 200)
+
+        contours, hierarchy = cv2.findContours(canny,
+           cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # print("Number of Contours = " ,len(contours))
+        # cv2.imshow('Canny Edges', canny)
+        
+        # combining contours
+        # contours = np.vstack([contours[5], contours[6]])
+        # contours = np.vstack(contours)
+
+        # for c in contours:
+        #     hull = cv2.convexHull(c)
+
+        # cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+        #create an empty image for contours
+        img_contours = np.zeros(img.shape)
+        # draw the contours on the empty image
+        cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+        #save image
+        cv2.imwrite(output_filename,img_contours)
+        cv2.imshow('Contours', img_contours)
+
+        # for imshow
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def action_C(input_filename, output_filename):
+        ...
+
+    button_1_tk = tk.Button(p_left, text='A', command=lambda: action_A(filename, '/tmp/contour.png'))
     p_left.add(button_1_tk, sticky=tk.NW)
-    button_2_tk = tk.Button(p_left, text='B', command=action_B)
+    button_2_tk = tk.Button(p_left, text='B', command=lambda: action_B(filename, '/tmp/contour.png'))
+    button_3_tk = tk.Button(p_left, text='C', command=lambda: action_C(filename, '/tmp/contour.png'))
     p_left.add(button_2_tk, sticky=tk.NW)
 
     p2 = tk.PanedWindow(p1, orient=tk.VERTICAL)
     p1.add(p2)
 
-    filename = '/tmp/naturaleza.webp'
+    # filename = '/tmp/naturaleza.webp'
+    # filename = '/tmp/naturaleza.png'
+    filename = '/tmp/naturaleza-flat.png'
+    # filename = '/tmp/mountains.png'
 
     canvas_width, canvas_height = 300, 300
 
